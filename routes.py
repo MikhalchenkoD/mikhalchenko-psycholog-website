e802ee59d816039d7bf48974ea6713ca58cdd247
MikhalchenkoD <dmikhalchenko.03@gmail.com>
2023-07-28 22:28:31 +0600

    v1.1
    I'm refactor code and create func send_user_to_bot. All functions working normal

--- routes.py
+++ routes.py
@@ -9,12 +9,6 @@ CORS(app)
 create_database_tables()
 
 
-@app.route('/client', methods=['GET'])
-def get_newest_item():
-    newest_item = session.query(Clients).order_by(Clients.id.desc()).first()
-    return newest_item.to_string() if newest_item else ""
-
-
 @app.route('/client', methods=['POST'])
 def add_client():
     name = request.form.getlist('name')
@@ -27,24 +21,31 @@ def add_client():
     result = add_client_to_db(name[0], email[0], message[0])
 
     if isinstance(result, bool) and result:
-        telegram_token = "6186492434:AAEGjP1ivaUaGMheq6-O-Y0Pay6Dv3SFRXo"  # Замените на свой API токен бота
-        chat_id = "1447559272"  # Замените на ID вашего чата, куда будут отправляться уведомления
-
-        message_text = f"Новый клиент: {name[0]}\nEmail: {email[0]}\nСообщение: {message[0]}"
-        url = f"https://api.telegram.org/bot{telegram_token}/sendMessage"
-        data = {
-            "chat_id": chat_id,
-            "text": message_text
-        }
-        response = requests.post(url, data=data)
+        response = send_user_to_bot(name[0], email[0], message[0])
 
         if response.ok:
             return f"Клиент {name[0]} успешно добавлен и отправлен в Telegram.", 201
         else:
             return f"Клиент {name[0]} успешно добавлен, но произошла ошибка при отправке в Telegram.", 201
 
-    else:
+    else: 
         return f"Ошибка при добавлении клиента: {result}", 500
+
+
+def send_user_to_bot(name: str, email: str, message: str):
+    telegram_token = "6186492434:AAEGjP1ivaUaGMheq6-O-Y0Pay6Dv3SFRXo"  # Замените на свой API токен бота
+    chat_id = "1447559272"  # Замените на ID вашего чата, куда будут отправляться уведомления
+
+    message_text = f"Новый клиент: {name}\nEmail: {email}\nСообщение: {message}"
+    url = f"https://api.telegram.org/bot{telegram_token}/sendMessage"
+    data = {
+        "chat_id": chat_id,
+        "text": message_text
+    }
+
+    response = requests.post(url, data=data)
+
+    return response
 
 
 if __name__ == "__main__":
